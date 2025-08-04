51acb90ea3b86eceb9993d46288073c63fd073fc
Bhupesh Kumar <bkumar_be23@thapar.edu>
2025-08-04 14:58:20 +0530

    completed

---
+++ Assignment 1/Question2.py
@@ -0,0 +1,24 @@
+# 2. Data Types and Type Conversion
+# Take input from the user and print the data type of the entered value.
+# Convert a string to an integer and perform arithmetic operations.
+# Explore implicit and explicit type conversions in Python.
+
+
+input1= input("Enter a number: ")
+print(type(input1))
+
+
+stringtointeger = "70"
+num=int(stringtointeger)
+print(num)
+
+x = 5    
+y = 2.5 
+z = x + y  
+print(z, type(z))
+
+n = "123"
+m = int(n) 
+print(m + 10)
+
+
