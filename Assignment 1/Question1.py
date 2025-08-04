2b86bc7e2fdaed341d4a3a978152e1869cf2d46e
Bhupesh Kumar <bkumar_be23@thapar.edu>
2025-08-04 14:55:43 +0530

    ai-ml acad

---
+++ Assignment 1/Question1.py
@@ -0,0 +1,21 @@
+# 1. Printing and Variables
+# Writea program to print "Hello, World!"
+# Declare variables of different data types (string, integer, float) and print their
+# values.
+# Swap the values of two variables without using a third variable.
+
+
+print("Hello, World!")  
+
+name ="bhupesh"
+age =25
+salary =50.21
+
+
+print(name, age, salary)
+
+a = 10
+b = 20
+a, b = b, a
+print(a,b)
+
